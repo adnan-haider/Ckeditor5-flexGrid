174767ab5d912df313b96ba404efaba8b7582295
Nouman Ul Haq <numankhaan4@gmail.com>
2022-10-06 22:46:56 +0500

    Updates

--- two-layout-grid/simplegrid.js
+++
@@ -1,9 +0,0 @@
-import SimpleGridEditing from './simplegridediting';
-import SimpleGridUI from './simplegridui';
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-
-export default class SimpleGrid extends Plugin {
-    static get requires() {
-        return [ SimpleGridEditing, SimpleGridUI ];
-    }
-}
